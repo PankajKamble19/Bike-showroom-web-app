685503cd873967ce5b94bf316a1e7ea1f2f2900f
Pankaj <03pankaj1911.k@gmail.com>
2026-09-01 20:45:06 +0530

    Fix BikeRoutes filename case

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth",    require("./routes/authRoutes"));
-app.use("/api/bikes",   require("./routes/bikeRoutes"));
+app.use("/api/bikes",   require("./routes/BikeRoutes"));
 app.use("/api/inquiries", require("./routes/inquiryRoutes"));
 app.use("/api/accessories",  require("./routes/accessoryRoutes"));
 app.use("/api/testimonials", require("./routes/testimonialRoutes"));
